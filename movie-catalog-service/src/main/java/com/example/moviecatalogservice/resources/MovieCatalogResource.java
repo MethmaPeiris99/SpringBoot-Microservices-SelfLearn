0f049791a6e2584fc99b285be32c0c03ee006757
Methma Peiris <methma34@gmail.com>
2021-09-21 14:12:27 +0530

    Implement the functionality to retrieve movie ratiing according to a movie id

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,12 +1,17 @@
 package com.example.moviecatalogservice.resources;
 
 import com.example.moviecatalogservice.models.CatalogItem;
+import com.example.moviecatalogservice.models.Movie;
+import com.example.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,10 +19,27 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Big Hero 6",
-                        "Movie about 6 heroes",
-                        10)
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Get all rated movie ids
+        // ASSUMPTION : ratings list is the response received from ratings-data API
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234",4),
+                new Rating("5678",3)
         );
+
+        // Get all movie details from movie-info-service for each movie id
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getMovieName(), "Movie about 6 heroes", rating.getMovieRating());
+        }).collect(Collectors.toList());
+
+        // Combine those data altogether
+//        return Collections.singletonList(
+//                new CatalogItem("Big Hero 6",
+//                        "Movie about 6 heroes",
+//                        10)
+//        );
     }
 }
