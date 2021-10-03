e463c1c7b70adf0a2ad1985617f46d03c721d5d9
Methma Peiris <methma34@gmail.com>
2021-10-03 10:04:15 +0530

    Creation of UserRating class

--- ratings-data-service/src/main/java/com/example/ratingsdataservice/resources/RatingsDataResource.java
+++ ratings-data-service/src/main/java/com/example/ratingsdataservice/resources/RatingsDataResource.java
@@ -1,6 +1,7 @@
 package com.example.ratingsdataservice.resources;
 
 import com.example.ratingsdataservice.models.Rating;
+import com.example.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,14 @@ public class RatingsDataResource {
     }
 
     @RequestMapping("users/{userId}")
-    public List<Rating> getUserRating(@PathVariable("userId")String userId) {
+    public UserRating getUserRating(@PathVariable("userId")String userId) {
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234",4),
                 new Rating("5678",10)
         );
-        return ratings;
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 }
 
